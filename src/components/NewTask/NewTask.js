251757cb9564f77a8cf487dc3d1fe1579e11cb7f
Daman Behl <damanbehl@gmail.com>
2021-07-22 15:20:13 -0400

    custom hook logic and implementation

--- src/components/NewTask/NewTask.js
+++ src/components/NewTask/NewTask.js
@@ -12,7 +12,7 @@ const NewTask = (props) => {
     setError(null);
     try {
       const response = await fetch(
-        'https://react-http-6b4a6.firebaseio.com/tasks.json',
+        'dummy link',
         {
           method: 'POST',
           body: JSON.stringify({ text: taskText }),
